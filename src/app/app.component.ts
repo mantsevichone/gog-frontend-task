a0c8ecc159a864e87df808a1bfd7ef14f44fd047
Artur Mantsevich <arturmantsevich@gmail.com>
2025-05-17 14:22:05 +0200

    feat: remove placeholder from app-root and use app-games

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { GamesComponent } from './games/games.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [GamesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
