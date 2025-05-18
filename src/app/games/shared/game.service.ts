5f94cdff81794fd8346c0cc540f8ddf07b1aaa03
Artur Mantsevich <arturmantsevich@gmail.com>
2025-05-18 13:45:33 +0200

    feat: take an owned game into account

--- src/app/games/shared/game.service.ts
+++ src/app/games/shared/game.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { GAMES } from './mock-games';
 import { GAME_OF_THE_WEEK } from './mock-week-game';
 
+// In real app it would be read from a user service
+const OWNED_GAME_IDS = [2];
+
 @Injectable({ providedIn: 'root' })
 export class GameService {
   getGames() {
@@ -11,5 +14,9 @@ export class GameService {
 
   getGameOfTheWeek() {
     return Promise.resolve(GAME_OF_THE_WEEK);
+  }
+
+  getOwnedGames() {
+    return Promise.resolve(OWNED_GAME_IDS);
   }
 }
