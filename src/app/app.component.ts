53e01f153c48cd1251b8aed66359bea885a7f676
Artur Mantsevich <arturmantsevich@gmail.com>
2025-05-18 21:14:20 +0200

    feat: apply all created components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import { GamesComponent } from './games/games.component';
+import { CartComponent } from './shared/cart/cart.component';
 
 @Component({
   selector: 'app-root',
-  imports: [GamesComponent],
+  imports: [GamesComponent, CartComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gog-frontend-task';
