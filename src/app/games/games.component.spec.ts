15d4310c4e4dd92242c6891666208d3ba84d9f52
Artur Mantsevich <arturmantsevich@gmail.com>
2025-05-19 22:40:50 +0200

    test: games component

--- src/app/games/games.component.spec.ts
+++ src/app/games/games.component.spec.ts
@@ -5,19 +5,41 @@ import { GamesComponent } from './games.component';
 describe('GamesComponent', () => {
   let component: GamesComponent;
   let fixture: ComponentFixture<GamesComponent>;
+  let element: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [GamesComponent]
-    })
-    .compileComponents();
+      imports: [GamesComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GamesComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    element = fixture.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render a list of games from the service', async () => {
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const games = element.querySelectorAll('.game');
+
+    expect(games.length).toBe(component.games.length);
+  });
+
+  it('should render a game of week', async () => {
+    expect(element.querySelector('h1')).toBeFalsy();
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const h1 = element.querySelector('h1');
+
+    expect(h1).toBeTruthy();
+    expect(h1?.textContent).toBe('GAME OF THE WEEK');
+
+    const image = element.querySelector(
+      `img[alt="${component.gameOfTheWeek?.name}"]`
+    );
+    expect(image).toBeTruthy();
   });
 });
